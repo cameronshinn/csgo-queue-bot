dde4cfa3397e07512b8544429698cb2198c0e61f
cameronshinn <cameron.shinn@hotmail.com>
2019-10-04 00:14:49 -0700

    Add comments

--- qbot.py
+++ qbot.py
@@ -15,10 +15,9 @@ GITHUB = 'github.com/cameronshinn/csgo-queue-bot'
 
 class QueueGuild:
     def __init__(self, guild):
-        self.guild = guild
-        self.queue = []
-        self.alertees = set()
-        self.commandVector = { 'q!help':     self.help_command,
+        self.guild = guild # Guild that class instance belongs to
+        self.queue = [] # Where users in queue are held
+        self.commandVector = { 'q!help':     self.help_command, # Map command strings to handler functions for easy lookup
                                'q!join':     self.join_command,
                                'q!leave':    self.leave_command,
                                'q!view':     self.view_command,
@@ -30,7 +29,7 @@ class QueueGuild:
         self.thumbnailUrl = THUMBNAIL
         self.color = LOGO_COLOR
         self.mapPool = MAP_POOL
-        self.mapsLeft = None
+        self.mapsLeft = None # Set to None when there is no ongoing draft
 
     @property
     def helpEmbed(self):
@@ -48,10 +47,10 @@ class QueueGuild:
 
     @property
     def queueEmbed(self):
-        if self.queue: # If there are members in the queue
+        if self.queue: # If there are users in the queue
             enumQueue = enumerate(self.queue, start=1)
             queueStr = ''.join(f'{enumUser[0]}. {enumUser[1].display_name}\n' for enumUser in enumQueue)
-        else:
+        else: # No users in queue
             queueStr = '_The queue is empty..._'
 
         embed = discord.Embed(title='__Players in queue for 10-mans__', description=queueStr, color=self.color)
@@ -59,7 +58,7 @@ class QueueGuild:
         return embed
 
     @property
-    def popflashEmbed(self): # Set as a property since the server name can change over time
+    def popflashEmbed(self): # Set with property decorator since the server name can change over time
         guildUrl = POPFLASH_URL + ''.join(char.lower() for char in str(self.guild) if char.isalpha() or char.isdigit())
         return discord.Embed(title="Popflash lobby is up!", description=guildUrl, color=self.color)
 
@@ -67,20 +66,21 @@ class QueueGuild:
     def aboutEmbed(self):
         embed = discord.Embed(title='__10-ManQ Queue Bot__', description='The definitive bot for setting up 10-man lobbies', color=self.color)
         embed.set_thumbnail(url=self.thumbnailUrl)
-        embed.add_field(name='This bot was made to give CS:GO players a convenient way to find Discord server members who want to play in-house pickup games', value='github.com/cameronshinn/csgo-queue-bot', inline=False)
+        embed.add_field(name='This bot was made to give CS:GO players a convenient way to find Discord server members who want to play in-house pickup games', value=GITHUB, inline=False)
         return embed
 
     async def join_command(self, message):
-        if message.author in self.queue:
+        if message.author in self.queue: # User already in queue
             embed = discord.Embed(title=f'**{message.author.display_name}** is already in the queue', color=self.color)
-        if len(self.queue) >= 10:
+        if len(self.queue) >= 10: # Queue full
             embed = discord.Embed(title=f'Unable to add **{message.author.display_name}**\nQueue is full _({len(self.queue)}/10)_', color=self.color)
-        else:
+        else: # Space available and not already in queue
             self.queue.append(message.author)
             embed = discord.Embed(title=f'**{message.author.display_name}** has been added to the queue _({len(self.queue)}/10)_', color=self.color)
 
         await message.channel.send(embed=embed)
 
+        # Check and pop queue if full
         if len(self.queue) == 10:
             userMentions = ''    
                 
@@ -125,32 +125,33 @@ class QueueGuild:
 
     async def draft_command(self, message):
         self.mapsLeft = copy.copy(self.mapPool) # Need to copy to preseve the original map pool
-        mapsLeftStr = ''.join(f'{i}. {m}\n' for i, m in enumerate(self.mapsLeft, 1)) # TODO: Add support for command
+        mapsLeftStr = ''.join(f'{i}. {m}\n' for i, m in enumerate(self.mapsLeft, 1))
         embed = discord.Embed(title=f'Draft has begun!', description=mapsLeftStr, color=self.color)
         await message.channel.send(embed=embed)
 
     async def ban_command(self, message):
-        csMap = message.content.split(' ', 1)[1]
+        csMap = message.content.split(' ', 1)[1] # Remove command from input
 
-        if csMap.isdigit() and int(csMap) > 0 and int(csMap) <= len(self.mapPool):
+        # Check if map number specified and convert to map name if so
+        if csMap.isdigit() and int(csMap) > 0 and int(csMap) <= len(self.mapPool): # Check if digit and in range
             i = int(csMap)
-            csMap = self.mapPool[i - 1]
+            csMap = self.mapPool[i - 1] # Convert to map name
 
-        if self.mapsLeft == None:
+        if self.mapsLeft == None: # Will only ever be None when there's no active draft
             embed = discord.Embed(title='Map draft has not started!', color=self.color)
-        elif csMap in self.mapsLeft:
+        elif csMap in self.mapsLeft: # Remove map from remaining
             self.mapsLeft.remove(csMap)
             mapsLeftStr = ''.join([f'{i}. {m}\n' if m in self.mapsLeft else f'{i}. ~~{m}~~\n' for i, m in enumerate(self.mapPool, 1)])
             embed = discord.Embed(title=f'{csMap} has been banned', description=mapsLeftStr, color=self.color)
-        elif csMap in self.mapPool:
+        elif csMap in self.mapPool: # Otherwise map must already be banned
             mapsLeftStr = ''.join([f'{i}. {m}\n' if m in self.mapsLeft else f'{i}. ~~{m}~~\n' for i, m in enumerate(self.mapPool, 1)])
             embed = discord.Embed(title=f'{csMap} has already been banned', description=mapsLeftStr, color=self.color)
-        else:
+        else: # Otherwise input is not map
             embed = discord.Embed(title=f'{csMap} is not a map', color=self.color)
             
-        if len(self.mapsLeft) == 1:
+        if len(self.mapsLeft) == 1: # End draft when no choices left
             embed = discord.Embed(title=f'We\'re going to {self.mapsLeft[0]}!', color=self.color)
-            self.mapsLeft = None
+            self.mapsLeft = None # Set to None when there is no ongoing draft
 
         await message.channel.send(embed=embed)
 
@@ -170,11 +171,11 @@ class QueueGuild:
 class QueueBot:
     def __init__(self, token):
         self.client = discord.Client()
-        self.token = token # Discord api bot token
+        self.token = token # Discord API bot token
         self.queueGuildDict = {} # Map guild name string to corresponding QueueGuild object
 
         @self.client.event
-        async def on_ready(): # Not sure if async is necessary here
+        async def on_ready(): # NOTE: Not sure if async is necessary here
             print(self.startupBanner)
 
             # Check and populate connected guilds on startup
