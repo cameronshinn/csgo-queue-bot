e1be2b5e1c3113da23dc7be0c375108d4e7f52e2
cameronshinn <cameron.shinn@hotmail.com>
2019-10-19 17:47:07 -0700

    Fix about embed

--- qbot/qbot.py
+++ qbot/qbot.py
@@ -152,9 +152,9 @@ class QueueGuild:
     def about_embed(self):
         embed = discord.Embed(title='__10-ManQ Queue Bot__', description='The definitive bot for setting up 10-man lobbies', color=self.color)
         embed.set_thumbnail(url=self.thumbnail_url)
-        embed.add_field(name='This bot gives players a convenient way to find Discord server members who want to play in-house pickup games of CS:GO', value=GITHUB, inline=False)
-        embed.add_field(name='[Join our support server here](https://discordapp.com/invite/tskeyDA)')
-        embed.add_field(name='[Source code can be found here on GitHub](https://github.com/cameronshinn/csgo-queue-bot)')
+        embed.add_field(name='This bot gives players a convenient way to find Discord server members who want to play in-house pickup games of CS:GO', value='\u200b', inline=False)
+        embed.add_field(name='[Join our support server here](https://discordapp.com/invite/tskeyDA)', value='\u200b', inline=False)
+        embed.add_field(name='[Source code can be found here on GitHub](https://github.com/cameronshinn/csgo-queue-bot)', value='\u200b', inline=False)
         return embed
 
     async def join_command(self, message):
