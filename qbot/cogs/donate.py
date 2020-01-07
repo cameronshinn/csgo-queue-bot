78c15498051036d7e2b0440ebfa61eb6b20b68f8
apjohnson12 <57375639+apjohnson12@users.noreply.github.com>
2020-01-06 22:02:07 -0600

    Update donate.py
    got rid of unneeded function

--- qbot/cogs/donate.py
+++ qbot/cogs/donate.py
@@ -15,16 +15,12 @@ class DonateCog(commands.Cog):
         self.color = color
         self.donate_token = donate_token
 
-    def get_donation_url(self, guild):
-        """ Generate Donation link based on specific bot instance """
-        return self.donate_url
-
     async def cog_before_invoke(self, ctx):
         """ Trigger typing at the start of every command """
         await ctx.trigger_typing()
 
     @commands.command(brief='Link the server\'s designated Donation link')
     async def donate(self, ctx):
-        description = f'[Link here]({self.get_donation_url(ctx.guild)})'
+        description = f'[Link here]({self.donate_url(ctx.guild)})'
         embed = discord.Embed(title="Feel free to donate here: ", description=description, color=self.color)
         await ctx.send(embed=embed)
