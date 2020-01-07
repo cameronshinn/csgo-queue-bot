992e5f5abedf790cd296aecfd49398c65e9298db
Andrew Johnson <apjohnson12@crimson.ua.edu>
2020-01-06 21:42:10 -0600

    made a donate command

---
+++ qbot/cogs/donate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# popflash.py
+# cameronshinn
+
+import discord
+from discord.ext import commands
+
+
+class DonateCog(commands.Cog):
+    """ Cog to manage interactions with Donate Link """
+
+    def __init__(self, bot, color, donate_token):
+        """ Set attributes """
+        self.bot = bot
+        self.color = color
+        self.donate_token = donate_token
+
+    def get_donation_url(self, guild):
+        """ Generate Donation link based on specific bot instance """
+        return self.donate_token
+
+    async def cog_before_invoke(self, ctx):
+        """ Trigger typing at the start of every command """
+        await ctx.trigger_typing()
+
+    @commands.command(brief='Link the server\'s designated Donation link')
+    async def donate(self, ctx):
+        description = f'[Link here]({self.get_donation_url(ctx.guild)})'
+        embed = discord.Embed(title="Feel free to donate here: ", description=description, color=self.color)
+        await ctx.send(embed=embed)
--- qbot/qbot.py
+++ qbot/qbot.py
@@ -9,12 +9,13 @@ from cogs.mapdraft import MapDraftCog
 from cogs.popflash import PopflashCog
 from cogs.queue import QueueCog
 from cogs.teamdraft import TeamDraftCog
+from cogs.donate import DonateCog
 import discord
 from discord.ext import commands
 
 BOT_COLOR = 0x0D61B7
 
-def run(discord_token, dbl_token=None):
+def run(discord_token, dbl_token=None, donate_token=None):
     """ Create the bot, add the cogs and run it """
     bot = commands.Bot(command_prefix=('q!', 'Q!'), case_insensitive=True)
     bot.add_cog(ConsoleCog(bot))
@@ -23,8 +24,12 @@ def run(discord_token, dbl_token=None):
     bot.add_cog(TeamDraftCog(bot, BOT_COLOR))
     bot.add_cog(MapDraftCog(bot, BOT_COLOR))
     bot.add_cog(PopflashCog(bot, BOT_COLOR))
+    
 
     if dbl_token:
         bot.add_cog(DblCog(bot, dbl_token))
+    
+    if donate_token:
+        bot.add_cog(DonateCog(bot, BOT_COLOR, donate_token))
 
     bot.run(discord_token)
