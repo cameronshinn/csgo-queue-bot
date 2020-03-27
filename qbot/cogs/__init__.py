2e12c73240afc59ec87eafe80d66289f7ff7ae26
cameronshinn <cameron.shinn@hotmail.com>
2020-03-27 00:35:18 -0700

    Add init file for easier importing

---
+++ qbot/cogs/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# __init__.py
+# cameronshinn
+
+from .cacher import CacherCog
+from .console import ConsoleCog
+from .dbl import DblCog
+from .donate import DonateCog
+from .help import HelpCog
+from .mapdraft import MapDraftCog
+from .popflash import PopflashCog
+from .queue import QueueCog
+from .teamdraft import TeamDraftCog
--- qbot/qbot.py
+++ qbot/qbot.py
@@ -3,16 +3,7 @@
 # cameronshinn
 
 from discord.ext import commands
-
-from cogs.cacher import CacherCog
-from cogs.console import ConsoleCog
-from cogs.dbl import DblCog
-from cogs.help import HelpCog
-from cogs.mapdraft import MapDraftCog
-from cogs.popflash import PopflashCog
-from cogs.queue import QueueCog
-from cogs.teamdraft import TeamDraftCog
-from cogs.donate import DonateCog
+import cogs
 
 BOT_COLOR = 0x0D61B7
 DATA_PATH = 'guild_data.json'
@@ -21,21 +12,21 @@ DATA_PATH = 'guild_data.json'
 def run(discord_token, dbl_token=None, donate_url=None, generic=False):
     """ Create the bot, add the cogs and run it. """
     bot = commands.Bot(command_prefix=('q!', 'Q!'), case_insensitive=True)
-    bot.add_cog(CacherCog(bot, DATA_PATH))
-    bot.add_cog(ConsoleCog(bot))
-    bot.add_cog(HelpCog(bot, BOT_COLOR))
-    bot.add_cog(QueueCog(bot, BOT_COLOR))
-    bot.add_cog(TeamDraftCog(bot, BOT_COLOR))
+    bot.add_cog(cogs.CacherCog(bot, DATA_PATH))
+    bot.add_cog(cogs.ConsoleCog(bot))
+    bot.add_cog(cogs.HelpCog(bot, BOT_COLOR))
+    bot.add_cog(cogs.QueueCog(bot, BOT_COLOR))
+    bot.add_cog(cogs.TeamDraftCog(bot, BOT_COLOR))
 
     if not generic:
-        bot.add_cog(MapDraftCog(bot, BOT_COLOR))
-        bot.add_cog(PopflashCog(bot, BOT_COLOR))
+        bot.add_cog(cogs.MapDraftCog(bot, BOT_COLOR))
+        bot.add_cog(cogs.PopflashCog(bot, BOT_COLOR))
         bot.remove_command('cap')
 
     if dbl_token:
-        bot.add_cog(DblCog(bot, dbl_token))
+        bot.add_cog(cogs.DblCog(bot, dbl_token))
 
     if donate_url:
-        bot.add_cog(DonateCog(bot, BOT_COLOR, donate_url))
+        bot.add_cog(cogs.DonateCog(bot, BOT_COLOR, donate_url))
 
     bot.run(discord_token)
